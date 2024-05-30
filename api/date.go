c1509070bbfb93e82605ac1d0967459d8a201f15
eabramov1993 <eabramov1993@gmail.com>
2024-05-30 19:00:43 +0500

    a

--- api/date.go
+++ api/date.go
@@ -2,24 +2,30 @@ package api
 
 import (
 	"encoding/json"
+	"fmt"
 	"groupie-trackers/internal/models"
 	"net/http"
 )
 
-func GetDate() ([]models.Date, error) {
-	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/dates")
+func GetDates(artistID int) ([]models.Date, error) {
+	url := fmt.Sprintf("https://groupietrackers.herokuapp.com/api/dates/%d", artistID)
+	resp, err := http.Get(url)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to get dates: %v", err)
 	}
 
 	defer resp.Body.Close()
 
-	var Date []models.Date
+	var datesRespons models.DateResponse
 
-	err = json.NewDecoder(resp.Body).Decode(&Date)
+	err = json.NewDecoder(resp.Body).Decode(&datesRespons)
 	if err != nil {
-		return nil, err
+		return nil, fmt.Errorf("failed to decode dates: %v", err)
 	}
 
-	return Date, nil
+	var dates []models.Date
+	for _, dataTime := range datesRespons.Dates {
+		dates = append(dates, models.Date{DateTime: dataTime})
+	}
+	return dates, nil
 }
